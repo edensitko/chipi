574388530b548e8c2c43149e9d37be0293896df9
Eden sitko <123290504+edensitko@users.noreply.github.com>
2025-03-23 21:42:52 +0200

    Update

--- chipi/lib/constants/api_consts.dart
+++ chipi/lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "api";
